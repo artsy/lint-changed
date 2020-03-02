06dbadfe1a1b98571832a430cad491beaf812fe2
Justin Bennett <zephraph@gmail.com>
2020-03-01 20:23:04 -0500

    Run lint-changed commands on diff

--- src/lint-changed.ts
+++ src/lint-changed.ts
@@ -4,6 +4,20 @@ import { exec } from "child_process";
 import fs from "fs";
 import µ from "micromatch";
 import pEach from "p-each-series";
+import to from "await-to-js";
+import { red, yellow } from "kleur";
+
+const log = (msg: string) => {
+  console.log("[lint-changed]:", msg);
+};
+
+const warn = (msg: string) => {
+  console.warn(yellow(`[lint-changed]: ${msg}`));
+};
+
+const error = (msg: string) => {
+  console.error(red(`[lint-changed]: ${msg}`));
+};
 
 interface PkgConfig {
   ["lint-changed"]?: {
@@ -26,31 +40,57 @@ async function runCommand(cmdString: string) {
 
 const git = (args: string) => runCommand(`git ${args}`);
 
-const getBranch = () => git("rev-parse --abbrev-ref HEAD");
+const getBranch = () => git("rev-parse --abbrev-ref HEAD").then(b => b.trim());
 const getLastTag = () => git("describe --tags --abbrev=0 HEAD^");
-const getChangedFiles = (event: string) => git(`diff --name-only ${event}`);
+const getChangedFiles = (event: string) =>
+  git(`diff --name-only ${event}`).then(r => r.split("\n").filter(n => !!n));
 
 export async function lintChanged() {
   const lintConfig = pkg["lint-changed"];
   if (!lintConfig) {
-    console.warn("No `lint-config` found in package.json");
+    warn("No `lint-config` found in package.json");
   } else {
     const branch = await getBranch();
     let changedFiles: string[] = [];
 
     // Determine changed files based on branch
     if (branch === "master") {
-      changedFiles.push(await getChangedFiles(`${await getLastTag()}..HEAD`));
+      const [tagFetchError, lastTag] = await to(getLastTag());
+      if (tagFetchError) {
+        error("Unable to retrieve last tag");
+        process.exit(1);
+      }
+      const [changedFilesError, changedFilesList] = await to(
+        getChangedFiles(`${lastTag}..HEAD`)
+      );
+      if (changedFilesError || changedFilesList === undefined) {
+        error("Unable to get changed files since last tag");
+        process.exit(1);
+      }
+      changedFiles.concat(changedFilesList);
     } else {
-      changedFiles.push(await getChangedFiles("origin/master"));
+      const [changedFilesError, changedFilesList] = await to(
+        getChangedFiles(`origin/master`)
+      );
+      if (changedFilesError || changedFilesList === undefined) {
+        error("Unable to get changed files since origin/master");
+        process.exit(1);
+      }
+      changedFiles = changedFiles.concat(changedFilesList);
     }
     if (changedFiles.length === 0) return;
 
     Object.entries(lintConfig)
       .map(([glob, cmds]) => [glob, Array.isArray(cmds) ? cmds : [cmds]])
       .forEach(async ([glob, commands]) => {
-        µ(changedFiles, glob).forEach(file => {
-          pEach(commands, command => runCommand(`${command} ${file}`));
+        µ(changedFiles, glob, {
+          dot: true,
+          matchBase: !glob.includes("/")
+        }).forEach(file => {
+          pEach(commands, command => {
+            log(`${command} ${file}`);
+            return runCommand(`${command} ${file}`).then(o => log(o));
+          });
         });
       });
   }
